ee1a2a6d70b42e1b51d193abfc27af3d96bd0be1
lionel-git <lionel.desorme@free.fr>
2019-05-10 20:19:43 +0200

    Init Commit

---
+++ TestNuget/Worker.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using Google.Protobuf;
+
+namespace TestNuget
+{
+    public class Worker
+    {
+        public int DoSumLength( int a, int b)
+        {
+            var x = ByteString.CopyFrom("toto", Encoding.ASCII);
+            return a + b + x.Length;
+        }
+    }
+}
---
+++ TestRefNugetFW/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TestNuget;
+
+namespace TestRefNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var w = new Worker();
+            int r = w.DoSumLength(5, 6);
+            Console.WriteLine(r);
+        }
+    }
+}
