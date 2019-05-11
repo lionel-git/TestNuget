5707d4182d95f3135fa61d7205548a7d72848cd9
lionel-git <lionel.desorme@free.fr>
2019-05-11 13:48:41 +0200

    update clean

--- TestNuget/build_nuget.ps1
+++ TestNuget/build_nuget.ps1
@@ -1,3 +1,4 @@
-dotnet.exe clean -v n
+# Test Nuget generation
+dotnet.exe clean -v n -c Release
 dotnet.exe build -v n -c Release --force
 dotnet.exe pack -v n -c Release --force -o \\freebox\samsung\Nugets\
